dd9edc0f73dcfd9c0d7e9aaaf064472d695514de
ghamsterjer-cyber <ghamsterjer@gmail.com>
2026-02-18 16:06:30 +0300

    Create config.ts

---
+++ proxigram/src/firebase/config.ts
@@ -0,0 +1,8 @@
+
+export const firebaseConfig = {
+  "projectId": "lhkotkosh",
+  "appId": "1:927942475660:web:f95c7a65926e23dd0f20a9",
+  "apiKey": "AIzaSyA0yo2JV-JXrk0LIu1zow_gJsmCwVTVFFI",
+  "authDomain": "lhkotkosh.firebaseapp.com",
+  "messagingSenderId": "927942475660"
+};
