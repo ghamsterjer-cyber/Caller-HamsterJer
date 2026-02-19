5967961504bc2ad45d58c03c8fb460f9c024315c
ghamsterjer-cyber <ghamsterjer@gmail.com>
2026-02-19 11:10:34 +0300

    Update next.config.ts

--- proxigram/next.config.ts
+++ proxigram/next.config.ts
@@ -2,32 +2,10 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* Базовый путь приложения */
   basePath: '/proxigram',
-  /* Отключаем статический экспорт, чтобы работал серверный прокси */
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   images: {
     unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-  },
+  }
 };
 
 export default nextConfig;
