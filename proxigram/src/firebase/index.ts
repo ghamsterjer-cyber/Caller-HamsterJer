06e63afe82fd922038cd3b71d7e0979ceba4f633
ghamsterjer-cyber <ghamsterjer@gmail.com>
2026-02-19 10:58:17 +0300

    Create index.ts

---
+++ proxigram/src/firebase/index.ts
@@ -0,0 +1,27 @@
+
+'use client';
+import { firebaseConfig } from './config';
+import { initializeApp, getApps, getApp, FirebaseApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore'
+
+export function initializeFirebase() {
+  if (!getApps().length) {
+    const firebaseApp = initializeApp(firebaseConfig);
+    return getSdks(firebaseApp);
+  }
+  return getSdks(getApp());
+}
+
+export function getSdks(firebaseApp: FirebaseApp) {
+  return {
+    firebaseApp,
+    auth: getAuth(firebaseApp),
+    firestore: getFirestore(firebaseApp)
+  };
+}
+
+export * from './provider';
+export * from './client-provider';
+export * from './firestore/use-collection';
+export * from './firestore/use-doc';
