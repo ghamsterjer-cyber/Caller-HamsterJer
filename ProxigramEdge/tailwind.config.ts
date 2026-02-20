f46ec90857dc89c8467cd6af41e5131b78895cb6
ghamsterjer-cyber <ghamsterjer@gmail.com>
2026-02-20 11:17:55 +0300

    Update tailwind.config.ts

--- ProxigramEdge/tailwind.config.ts
+++ ProxigramEdge/tailwind.config.ts
@@ -1,14 +1,15 @@
-import type { Config } from "tailwindcss";
-
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       colors: {
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
         primary: {
@@ -16,13 +17,7 @@ const config: Config = {
           foreground: "hsl(var(--primary-foreground))",
         },
       },
-      borderRadius: {
-        lg: "var(--radius)",
-        md: "calc(var(--radius) - 2px)",
-        sm: "calc(var(--radius) - 4px)",
-      },
     },
   },
   plugins: [],
-};
-export default config;
+}
